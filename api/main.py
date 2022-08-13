74b422bb21a1451ccfb22dbc43fed9d0008276c4
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-13 17:28:12 -0500

    expand app args

--- api/main.py
+++ api/main.py
@@ -4,4 +4,8 @@ from starlite.plugins.sql_alchemy import SQLAlchemyPlugin
 from .controllers import route_handlers
 from .openapi import openapi_config
 
-app = Starlite(route_handlers=route_handlers, openapi_config=openapi_config, plugins=[SQLAlchemyPlugin()])
+app = Starlite(
+    route_handlers=route_handlers,
+    openapi_config=openapi_config,
+    plugins=[SQLAlchemyPlugin()]
+)
