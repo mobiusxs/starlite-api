16b812f8bfc8c7648b0960cb6444565f5c411fe4
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-14 12:47:39 -0500

    refactor route_handlers

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
 from starlite import Starlite
 from starlite.plugins.sql_alchemy import SQLAlchemyPlugin
 
-from .controllers import route_handlers
 from .openapi import openapi_config
+from .resources.item.views import ItemController
 
 app = Starlite(
-    route_handlers=route_handlers,
     openapi_config=openapi_config,
-    plugins=[SQLAlchemyPlugin()]
+    plugins=[SQLAlchemyPlugin()],
+    route_handlers=[
+        ItemController
+    ],
 )
